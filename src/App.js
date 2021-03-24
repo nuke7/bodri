3fa384f8e435a3542194071ad9a9c348735f6c84
nuke7 <gombos.marton@icloud.com>
2021-03-24 12:12:51 +0100

    started the task

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Veterinarian admin - clients</h1>
+      <input type="text" name="name" id="name" />
+      <button>search</button>
+    </div>
+  );
+};
+
+export default App;
