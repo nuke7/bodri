d75d7d7006add22fd02d955f2cd9fa5744b36d77
nuke7 <gombos.marton@icloud.com>
2021-03-24 13:21:09 +0100

    pet buttons and post works

--- src/components/Client.jsx
+++ src/components/Client.jsx
@@ -1,6 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
+let fd = {};
 
 export const Client = (props) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [message, setMessage] = useState("");
+
+  const uploadFunc = (e) => {
+    setIsLoading(true);
+    console.log(JSON.stringify(fd));
+    fetch("/api/pets", {
+      method: "POST",
+      /* mode: "cors", */
+      /*   headers: {
+				"Content-Type": "multipart/form-data",
+			}, */
+      body: JSON.stringify(fd),
+    })
+      .then((response) => {
+        /* console.log(response); */
+        return response.json();
+      })
+      .then((resp) => {
+        if (resp.success === true) {
+          setMessage("true");
+        } else {
+          setMessage("false");
+        }
+        console.log(message);
+      })
+      .then(() => setIsLoading(false));
+  };
+
   return (
     <div style={{ border: "1px solid black", padding: "1rem", margin: "1rem" }}>
       {props.data.name}
@@ -8,7 +39,18 @@ export const Client = (props) => {
         return (
           <div style={{ border: "1px solid black", padding: "1rem", margin: "1rem" }}>
             {pet.name} ({pet.animal}) - Vaccinated:{" "}
-            <button>{pet.isVaccinated ? "true" : "false"}</button>
+            <button
+              onClick={(e) => {
+                console.log(e.target.value);
+                fd = {
+                  name: pet.name,
+                  isVaccinated: !pet.isVaccinated,
+                };
+                uploadFunc();
+                pet.isVaccinated = !pet.isVaccinated;
+              }}>
+              {isLoading ? "..." : pet.isVaccinated ? "true" : "false"}
+            </button>
           </div>
         );
       })}
